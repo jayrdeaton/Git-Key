8295da23eac37dcdb5d44837173ec8942085c1eb
jaydeaton <jayrdeaton@gmail.com>
2019-04-18 23:50:25 -0500

    added reset to program

--- src/program/index.js
+++ src/program/index.js
@@ -1,6 +1,6 @@
 const { command, option } = require('termkit'),
   cosmetic = require('cosmetic'),
-  { add, config } = require('actions'),
+  { add, config, reset } = require('actions'),
   { printError } = require('../consoleIO'),
   { version } = require('../../package.json');
 
@@ -14,21 +14,24 @@ const program = command('git-key')
   .commands([
     // add
     command('add')
-    .description('add an ssh key')
-    .options([
-      option('f', 'force', null, 'overwrite existing key'),
-      option('n', 'name', '<name>', 'supply file name, default id_rsa'),
-      option('p', 'passphrase', '<passphrase>', 'protect key with passphrase'),
-    ])
-    .action(async (options) => await add(options)),
+      .description('add an ssh key')
+      .options([
+        option('f', 'force', null, 'overwrite existing key'),
+        option('n', 'name', '<name>', 'supply file name, default id_rsa'),
+        option('p', 'passphrase', '<passphrase>', 'protect key with passphrase'),
+      ])
+      .action(async (options) => await add(options)),
     // config
     command('config')
-    .description('configure git-key')
-    .options([
-      // option('d', 'directory', '<directory>', 'ssh key directory'),
-      option('u', 'username', '<username>', 'github username')
-    ])
-    .action(async (options) => await config(options)),
+      .description('configure git-key')
+      .options([
+        // option('d', 'directory', '<directory>', 'ssh key directory'),
+        option('u', 'username', '<username>', 'github username')
+      ])
+      .action(async (options) => await config(options)),
+    command('reset')
+      .description('reset config file')
+      .action(() => reset())
   ]);
 
 module.exports = program;
