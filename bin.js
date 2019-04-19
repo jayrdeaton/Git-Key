2874e2888083d92ad0eea41bb3b33ad78253f5d6
jaydeaton <jayrdeaton@gmail.com>
2019-04-18 23:56:23 -0500

    reconfigured run method

---
+++ bin.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const { consoleIO, program } = require('./'),
+  { printError } = consoleIO;
+
+const run = async(args) => {
+  try {
+    await program.parse(args);
+  } catch(err) {
+    printError(err);
+  };
+};
+
+run(process.argv);
