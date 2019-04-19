d6f91ae593653f01daeb500eadb9d7116225a344
jaydeaton <jayrdeaton@gmail.com>
2019-04-18 23:44:29 -0500

    added add action

---
+++ src/actions/add.js
@@ -0,0 +1,16 @@
+const { existsSync, unlinkSync } = require('fs'),
+  cosmetic = require('cosmetic');
+
+module.exports = async ({ filename, force, passphrase }) => {
+  if (!filename) filename = 'id_rsa';
+  if (force) {
+    if (existsSync(`${dir}/${filename}`)) unlinkSync(`${dir}/${filename}`);
+    if (existsSync(`${dir}/${filename}.pub`)) unlinkSync(`${dir}/${filename}.pub`);
+  };
+  try {
+    await controller.create(filename, passphrase);
+    console.log(`${cosmetic.green('success:')} created a new ssh key ${cosmetic.cyan(filename)}`));
+  } catch(err) {
+    console.log(cosmetic.red(`${err.name}:`), err.message);
+  };
+};
