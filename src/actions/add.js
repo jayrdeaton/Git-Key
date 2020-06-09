ae0123934a1bc3d26cfdbbe7a34208cc4eab70d4
Jay Deaton <jaydeaton@Jays-MacBook-Pro.local>
2020-06-09 17:02:13 -0500

    cleanup

--- src/actions/add.js
+++ src/actions/add.js
@@ -1,17 +1,17 @@
 const { existsSync, unlinkSync } = require('fs'),
   cosmetic = require('cosmetic'),
-  { createKey } = require('../helpers');
+  { createKey } = require('../helpers')
 
 module.exports = async ({ filename, force, passphrase }) => {
-  if (!filename) filename = 'id_rsa';
+  if (!filename) filename = 'id_rsa'
   if (force) {
-    if (existsSync(`${dir}/${filename}`)) unlinkSync(`${dir}/${filename}`);
-    if (existsSync(`${dir}/${filename}.pub`)) unlinkSync(`${dir}/${filename}.pub`);
-  };
+    if (existsSync(`${dir}/${filename}`)) unlinkSync(`${dir}/${filename}`)
+    if (existsSync(`${dir}/${filename}.pub`)) unlinkSync(`${dir}/${filename}.pub`)
+  }
   try {
-    await createKey(filename, passphrase);
-    console.log(`${cosmetic.green('success:')} created a new ssh key ${cosmetic.cyan(filename)}`);
+    await createKey(filename, passphrase)
+    console.log(`${cosmetic.green('success:')} created a new ssh key ${cosmetic.cyan(filename)}`)
   } catch(err) {
-    console.log(cosmetic.red(`${err.name}:`), err.message);
-  };
-};
+    console.log(cosmetic.red(`${err.name}:`), err.message)
+  }
+}
